9fe11dbd2b9519d01bedfc0d35786cc8b10243d1
Jubin Savla <jubinmr@gmail.com>
2025-10-15 15:04:55 +1100

    fixing icon path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,12 +24,12 @@ export const metadata: Metadata = {
   description: siteConfig.description,
   icons: {
     icon: [
-      { url: "/favicon.svg", type: "image/svg+xml" },
-      { url: "/favicon.ico", sizes: "32x32", type: "image/x-icon" },
-      { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" }
+      { url: "./favicon.svg", type: "image/svg+xml" },
+      { url: "./favicon.ico", sizes: "32x32", type: "image/x-icon" },
+      { url: "./favicon-96x96.png", sizes: "96x96", type: "image/png" }
     ],
     apple: {
-      url: "/apple-touch-icon.png",
+      url: "./apple-touch-icon.png",
       sizes: "180x180",
       type: "image/png"
     }
